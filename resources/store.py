0ced8b72694d46a8d2a6dabf75b226d24efeb748
alon galperin <alonga90@gmail.com>
2018-12-05 17:29:10 +0200

    Continue checking

--- resources/store.py
+++ resources/store.py
@@ -30,4 +30,5 @@ class Store(Resource):
 class StoreList(Resource):
     def get(self):
         # return "aa"
-        return {'stores': [store.json for store in StoreModel.query.all()]}
+        return StoreModel.query.all()
+        # return {'stores': [store.json for store in StoreModel.query.all()]}
